669e931be7dd263b0b452fef1247545d9bec0052
Alex <alexp@webefficient.ro>
2022-03-19 11:35:50 +0200

    Pagination & Style

--- ride-planner/src/components/BooksCard.tsx
+++ ride-planner/src/components/BooksCard.tsx
@@ -7,26 +7,28 @@ export interface IProps {
 
 const BooksCard = ({ book }: IProps) => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="" />
-      <Card.Body>
-        {book.name && <Card.Title>{book.name}</Card.Title>}
-        {book.authors && <Card.Text>{book.authors[0]}</Card.Text>}
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        {book.country && <ListGroupItem>{book.country}</ListGroupItem>}
-        {book.isbn && <ListGroupItem>{book.isbn}</ListGroupItem>}
-        {book.mediaType && <ListGroupItem>{book.mediaType}</ListGroupItem>}
-        {book.numberOfPages && (
-          <ListGroupItem>{book.numberOfPages}</ListGroupItem>
-        )}
-        {book.publisher && <ListGroupItem>{book.publisher}</ListGroupItem>}
-        {book.released && <ListGroupItem>{book.released}</ListGroupItem>}
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">{book.url}</Card.Link>
-      </Card.Body>
-    </Card>
+    <div className="col-12 col-sm-6 col-md-4 p-2">
+      <Card style={{height: "100%"}}>
+        <Card.Img variant="top" src="" />
+        <Card.Body>
+          {book.name && <Card.Title>{book.name}</Card.Title>}
+          {book.authors && <Card.Text>{book.authors[0]}</Card.Text>}
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          {book.country && <ListGroupItem>{book.country}</ListGroupItem>}
+          {book.isbn && <ListGroupItem>{book.isbn}</ListGroupItem>}
+          {book.mediaType && <ListGroupItem>{book.mediaType}</ListGroupItem>}
+          {book.numberOfPages && (
+            <ListGroupItem>{book.numberOfPages}</ListGroupItem>
+          )}
+          {book.publisher && <ListGroupItem>{book.publisher}</ListGroupItem>}
+          {book.released && <ListGroupItem>{book.released}</ListGroupItem>}
+        </ListGroup>
+        <Card.Body>
+          <Card.Link href="#">{book.url}</Card.Link>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
--- ride-planner/src/components/CharacterCard.tsx
+++ ride-planner/src/components/CharacterCard.tsx
@@ -7,64 +7,66 @@ export interface IProps {
 
 const CharacterCard = ({ character }: IProps) => {
   return (
-    <Card style={{ width: "18rem" }}>
-      <Card.Img variant="top" src="" />
-      <Card.Body>
-        <Card.Title>Character Aliases : {character.aliases}</Card.Title>
-      </Card.Body>
-      <ListGroup className="list-group-flush">
-        {character.name && (
-          <ListGroupItem>Character Name : {character.name}</ListGroupItem>
-        )}
-        {character.gender && (
-          <ListGroupItem>Character Gender : {character.gender}</ListGroupItem>
-        )}
-        {character.culture && (
-          <ListGroupItem>Character Culture :{character.culture}</ListGroupItem>
-        )}
-        {character.born && (
-          <ListGroupItem>Character Born : {character.born}</ListGroupItem>
-        )}
-        {character.died && (
-          <ListGroupItem>Character Died : {character.died}</ListGroupItem>
-        )}
-        {character.titles && (
-          <ListGroupItem>Character Titles : {character.titles}</ListGroupItem>
-        )}
-        {character.aliases && (
-          <ListGroupItem>Character Father : {character.father}</ListGroupItem>
-        )}
-        {character.mother && (
-          <ListGroupItem>Character Mother : {character.mother}</ListGroupItem>
-        )}
-        {character.spouse && (
-          <ListGroupItem>Character Spouse : {character.spouse}</ListGroupItem>
-        )}
-        {character.allegiances && (
-          <ListGroupItem>
-            Character Allegiances : {character.allegiances}
-          </ListGroupItem>
-        )}
-        {character.povBooks && (
-          <ListGroupItem>
-            Character PovBooks : {character.povBooks}
-          </ListGroupItem>
-        )}
-        {character.tvSeries && (
-          <ListGroupItem>
-            Character TvSeries : {character.tvSeries}
-          </ListGroupItem>
-        )}
-        {character.playedBy && (
-          <ListGroupItem>
-            Character PlayedBy : {character.playedBy}
-          </ListGroupItem>
-        )}
-      </ListGroup>
-      <Card.Body>
-        <Card.Link href="#">{character.url}</Card.Link>
-      </Card.Body>
-    </Card>
+    <div className="col-12 col-sm-6 col-md-4 p-2">
+      <Card style={{height: "100%"}}>
+        <Card.Img variant="top" src="" />
+        <Card.Body>
+          <Card.Title>Character Aliases : {character.aliases}</Card.Title>
+        </Card.Body>
+        <ListGroup className="list-group-flush">
+          {character.name && (
+            <ListGroupItem>Character Name : {character.name}</ListGroupItem>
+          )}
+          {character.gender && (
+            <ListGroupItem>Character Gender : {character.gender}</ListGroupItem>
+          )}
+          {character.culture && (
+            <ListGroupItem>Character Culture :{character.culture}</ListGroupItem>
+          )}
+          {character.born && (
+            <ListGroupItem>Character Born : {character.born}</ListGroupItem>
+          )}
+          {character.died && (
+            <ListGroupItem>Character Died : {character.died}</ListGroupItem>
+          )}
+          {character.titles && (
+            <ListGroupItem>Character Titles : {character.titles}</ListGroupItem>
+          )}
+          {character.aliases && (
+            <ListGroupItem>Character Father : {character.father}</ListGroupItem>
+          )}
+          {character.mother && (
+            <ListGroupItem>Character Mother : {character.mother}</ListGroupItem>
+          )}
+          {character.spouse && (
+            <ListGroupItem>Character Spouse : {character.spouse}</ListGroupItem>
+          )}
+          {character.allegiances && (
+            <ListGroupItem>
+              Character Allegiances : {character.allegiances}
+            </ListGroupItem>
+          )}
+          {character.povBooks && (
+            <ListGroupItem>
+              Character PovBooks : {character.povBooks}
+            </ListGroupItem>
+          )}
+          {character.tvSeries && (
+            <ListGroupItem>
+              Character TvSeries : {character.tvSeries}
+            </ListGroupItem>
+          )}
+          {character.playedBy && (
+            <ListGroupItem>
+              Character PlayedBy : {character.playedBy}
+            </ListGroupItem>
+          )}
+        </ListGroup>
+        <Card.Body>
+          <Card.Link href="#">{character.url}</Card.Link>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
